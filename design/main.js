2cf13a9e32d7af02bc5c6fbd67860520a7e7fbc6
Gaius388 <101121123+Gaius388@users.noreply.github.com>
2022-06-14 18:42:28 +0100

    Add files via upload

---
+++ design/main.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('btn');
+const err = document.getElementById('err');
+const input = document.getElementById('email');
+email.addEventListener('focus', () => {
+    err.style.display = 'none';
+    input.style.border = '1.5px solid var(--Blue)';
+})
+
+email.addEventListener('focusout', () => {
+    if (email.value <= 0 || email.value == null || email.value == undefined || (email.value.indexOf('.com') == -1)) {
+        err.style.display = 'flex';
+        input.style.border = '1.5px solid var(--LightRed)';
+
+    } else {
+        errMsg.style.display = 'none';
+    }
+})
+btn.addEventListener('click', () => {
+    if (email.value == null || email.value == undefined || email.value == '' || (email.value.indexOf('@') == -1)) {
+        err.style.display = 'flex';
+        input.style.border = '1.5px solid var(--LightRed)';
+    } else {
+        return true;
+    }
+})
